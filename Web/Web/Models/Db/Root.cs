bf0c45c4203892b4389f6e845971a698df6ac6d0
Slava <dex252rus@gmail.com>
2021-10-02 00:38:08 +1000

    Добавить CORS, обновить натсройки json. Добавить основную таблицу и её модель.

---
+++ Web/Web/Models/Db/Root.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using Dapper.Contrib.Extensions;
+using System.Xml;
+
+namespace Web.Models.Db
+{
+    [Dapper.Table("root")]
+    public class Classes
+    {
+        [Dapper.Key]
+        public decimal? Id { get; set; }
+
+        [Column("guid")]
+        public string Guid { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Computed]
+        public string Json { get; set; }
+
+        [Column("json")]
+        public XmlCDataSection CData
+        {
+            get
+            {
+                return new XmlDocument().CreateCDataSection(Json);
+            }
+            set
+            {
+                Json = value.Value;
+            }
+        }
+
+    }
+}
--- Web/Web/Startup.cs
+++ Web/Web/Startup.cs
@@ -1,14 +1,20 @@
 using Dapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Web.Middlewares;
+using Web.Repositories;
+using Web.Repositories.Connection;
 
 namespace Web
 {
     public class Startup
     {
+        const string SpecificOrigins = "SpecificOrigins";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -19,9 +25,25 @@ namespace Web
 
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddControllersWithViews();
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: SpecificOrigins,
+                    configurePolice =>
+                    {
+                        configurePolice
+                            .SetIsOriginAllowed(origin => true)
+                            .AllowAnyHeader()
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod();
+
+                    });
+            });
+
+            services.AddControllers().AddJsonOptions(options =>
+                                 options.JsonSerializerOptions.IgnoreNullValues = true);
+
             services.AddMvc();
-        }
+
             services.AddSingleton<IConnection, MySqlDbConnection>();
             services.AddScoped<ISmartRepository, SmartRepository>();
 
@@ -32,6 +54,7 @@ namespace Web
                     return Interceptor.Get(actionContext);
                 };
             });
+        }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -45,11 +68,14 @@ namespace Web
                 
                 app.UseHsts();
             }
+
             app.UseMiddleware<ExceptionMiddleware>();
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseCors(SpecificOrigins);
 
             app.UseAuthorization();
 
